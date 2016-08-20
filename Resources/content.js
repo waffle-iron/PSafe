a438a0f161b4d1fd115591519eda5356cb7ec51e
Matthew Shaw <matthewshaw0707@gmail.com>
2016-08-20 13:56:08 +1000

    Icon appears in password inputs

--- Resources/content.js
+++ Resources/content.js
@@ -1,6 +1,24 @@
 
+jQuery.fn.runSafe = function(param) {
+	alert("hello world!");
+}
+var imgURL = chrome.extension.getURL("icon.png");
 var passExists = $("input:password").first();
+
 if(passExists.length){
+	var parentHeight = passExists.parent().height()-5;
+	var parentColor = "#ffffff";
 	console.log("password field detected");
+
+	passExists.css({"background":"url('"+imgURL+"') "+parentColor+" right top no-repeat", "background-size":parentHeight});
+	passExists.mousemove(function(e){
+		var x = e.pageX - passExists.offset().left;
+		var y = e.pageY - passExists.offset().top;
+		if(x>passExists.width()-passExists.height()){
+				passExists.css("cursor","pointer");
+		}else if(passExists.css("cursor")=="pointer"){
+			passExists.css("cursor","");
+		}
+	});
 }
 
