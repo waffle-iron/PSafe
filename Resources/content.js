b6ab8d732c4833b69e15dcc38c6bf5958758738c
matthewshaw0707 <matthewshaw0707@gmail.com>
2016-08-22 19:17:52 -0400

    fix sizing and position of icon in password box

--- Resources/content.js
+++ Resources/content.js
@@ -4,7 +4,9 @@ var passExists = $("input:password").first();
 
 		$("form").submit(function(){
 			var user =$("input[name*='user']").val();
-			var pass =$("input[name*='pass']").val()
+		
+
+	var pass =$("input[name*='pass']").val()
 		});
 	
 
@@ -13,40 +15,59 @@ function checkPasswords(){
 	passExists = $("input:password");
 	if(passExists.length){
 		console.log(passExists);
-		var parentHeight = passExists.parent().height()-5;
+	
+
+	var parentHeight = passExists.outerHeight();
+		var parentWidth = passExists.outerWidth();
 		var parentColor = "#ffffff";
 		console.log("password field detected");
 
-		passExists.css({"background":"url('"+imgURL+"') "+parentColor+" right top no-repeat", "background-size":parentHeight});
+		
+
+passExists.css({"background":"url('"+imgURL+"') "+parentColor+" right center no-repeat","background-size":passExists.outerHeight()});
 		passExists.mousemove(function(e){
-			var x = e.pageX - passExists.offset().left;
-			var y = e.pageY - passExists.offset().top;
-			if(x>passExists.width()-passExists.height()){
-				passExists.css("cursor","pointer");
+			var x = 
+
+e.pageX - passExists.offset().left;
+	//		var y = e.pageY - passExists.offset().top;
+			if(x>parentWidth-parentHeight){
+				passExists.css
+
+("cursor","pointer");
 			}else if(passExists.css("cursor")=="pointer"){
 				passExists.css("cursor","");
 			}
 		});
 
-		passExists.click(function(e){
+		
+
+passExists.click(function(e){
 			passExists.trigger('keyup');
 			var x = e.pageX - passExists.offset().left;
-			var y = e.pageY - passExists.offset().top;
-			if(x>passExists.width()-passExists.height()){
+//			var y = e.pageY - passExists.offset().top;
+	
+
+		if(x>parentWidth-parentHeight){
 				chrome.extension.sendMessage({
 					type: "initiate", 
 					data: {
-						myProperty: "Hello"
+		
+
+				myProperty: "Hello"
 					}
 				});
 
 			}	
 		});
-		chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+		
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
 			passExists.focus()
 			passExists.val(msg.action);
 			passExists.keypress();
-			passExists.change();
+	
+
+		passExists.change();
 			passExists.keydown();
 			passExists.keyup();
 		});
@@ -55,7 +76,9 @@ function checkPasswords(){
 
 $(document).click(function(e) {
 	if(document.forms.length>0){
-		checkPasswords();
+	
+
+	checkPasswords();
 	}
 });
 
